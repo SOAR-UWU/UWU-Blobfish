1b1483ab743256173ec53c78df443080d24b0f6b
javin_e <javinenghp@gmail.com>
2023-11-27 22:54:05 +0800

    refactor: put startup char in constants

--- uwu_main/tests/arduino_interface_test.py
+++ uwu_main/tests/arduino_interface_test.py
@@ -6,6 +6,8 @@ BOARD_FQBN = "arduino:avr:mega"
 ARDUINO_PORT = "/dev/ttyUSB0"
 ARDUINO_TEST_INO = "arduino_test/arduino_test.ino"
 
+STARTUP_CHAR = b"S"
+
 
 def test_connection_init():
     ser = serial.Serial(ARDUINO_PORT)
@@ -14,7 +16,7 @@ def test_connection_init():
     print("Compiled, uploading...")
     subprocess.run(["arduino-cli", "upload", "-p", ARDUINO_PORT, "--fqbn", BOARD_FQBN, ARDUINO_TEST_INO])
     print("Uploaded to board.")
-    assert(interface.wait_for_connection(ser, b"S", 30))
+    assert(interface.wait_for_connection(ser, STARTUP_CHAR, 30))
 
 if __name__ == "__main__":
     test_connection_init()
