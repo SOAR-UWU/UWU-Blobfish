3b27fd2c51a4e223c0f3f56adf35c65eff45752e
javin_e <javinenghp@gmail.com>
2023-11-27 21:50:03 +0800

    test: init connection test

--- uwu_main/tests/arduino_interface_test.py
+++ uwu_main/tests/arduino_interface_test.py
@@ -1,5 +1,20 @@
 from import_context import arduino_interface as interface
-import pytest
+import serial
+import subprocess
+
+BOARD_FQBN = "arduino:avr:mega"
+ARDUINO_PORT = "/dev/ttyUSB0"
+ARDUINO_TEST_INO = "arduino_test/arduino_test.ino"
+
 
 def test_connection_init():
-    assert(interface.wait_for_connection(30))
+    ser = serial.Serial(ARDUINO_PORT)
+    print("Compiling arduino code...")
+    subprocess.run(["arduino-cli", "compile", "--fqbn", BOARD_FQBN, ARDUINO_TEST_INO])
+    print("Compiled, uploading...")
+    subprocess.run(["arduino-cli", "upload", "-p", ARDUINO_PORT, "--fqbn", BOARD_FQBN, ARDUINO_TEST_INO])
+    print("Uploaded to board.")
+    assert(interface.wait_for_connection(ser, b"S", 30))
+
+if __name__ == "__main__":
+    test_connection_init()
