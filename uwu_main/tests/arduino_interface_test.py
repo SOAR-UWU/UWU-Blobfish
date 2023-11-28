f307f3fa9c2f6bbf156edc5a388ee5839757dbeb
javin_e <javinenghp@gmail.com>
2023-11-29 00:21:11 +0800

    test: motor values transfer test

--- uwu_main/tests/arduino_interface_test.py
+++ uwu_main/tests/arduino_interface_test.py
@@ -1,3 +1,4 @@
+import pytest
 from import_context import arduino_interface as interface
 import serial
 import subprocess
@@ -8,15 +9,35 @@ ARDUINO_TEST_INO = "arduino_test/arduino_test.ino"
 
 STARTUP_CHAR = b"S"
 
-
-def test_connection_init():
-    ser = serial.Serial(ARDUINO_PORT)
+@pytest.fixture(scope="module")
+def startup():
     print("Compiling arduino code...")
     subprocess.run(["arduino-cli", "compile", "--fqbn", BOARD_FQBN, ARDUINO_TEST_INO])
-    print("Compiled, uploading...")
+    print("Compiled")
+
+def test_connection_init():
     subprocess.run(["arduino-cli", "upload", "-p", ARDUINO_PORT, "--fqbn", BOARD_FQBN, ARDUINO_TEST_INO])
     print("Uploaded to board.")
+    ser = serial.Serial(ARDUINO_PORT, timeout=30)
     assert(interface.wait_for_connection(ser, STARTUP_CHAR, 30))
+    ser.close()
+
+def test_send_motor_values():
+    subprocess.run(["arduino-cli", "upload", "-p", ARDUINO_PORT, "--fqbn", BOARD_FQBN, ARDUINO_TEST_INO])
+    print("Uploaded to board.")
+    ser = serial.Serial(ARDUINO_PORT, timeout = 5)
+    vals = [133, 434, 512, 123, 364, 125, 126]
+    interface.wait_for_connection(ser, STARTUP_CHAR, 30)
+    interface.write_motor_values(ser, vals)
+    recv = []
+
+    # Block until "M" is read
+    while ser.read() != b'M': pass
+
+    for _ in vals:
+        recv.append(interface.read_uint16(ser))
+    assert(recv == vals)
+    ser.close()
 
 if __name__ == "__main__":
-    test_connection_init()
+    test_send_motor_values()
--- uwu_main/tests/arduino_test/arduino_test.ino
+++ uwu_main/tests/arduino_test/arduino_test.ino
@@ -9,6 +9,9 @@ const int BaudRate = 9600;
 const int CharTimeout = 500;  // waittime for timeout between chars, in ms
 const char InitChar = 'S'; // setup char
 
+const int NumMotors = 7;
+uint8_t err = 0;
+
 // Functions
 void onError(uint8_t err_num);
 void awaitConnection(SimpleSerialProtocol ssp, char InitChar);
@@ -22,10 +25,28 @@ void setup() {
   awaitConnection(ssp, InitChar);
 }
 
-void loop() {}
+void loop() {
+  uint16_t motor_values[7];
+  bool received = recv_motor_values(ssp, motor_values);
+  if (received) {
+    ssp.writeChar('M');
+    for (auto val : motor_values) {
+      ssp.writeUnsignedInt16(val);
+    }
+  }
+}
 
 void onError(uint8_t err_num) {
-  BlinkPatterns::blink_error();
+  switch (err_num)
+  {
+  case ERROR_WAIT_FOR_BYTE_TIMEOUT:
+    BlinkPatterns::blink_timeout();   
+    err = 1;
+    break;
+  default:
+    BlinkPatterns::blink_error();
+    break;
+  }
 }
 
 void awaitConnection(SimpleSerialProtocol ssp, char InitChar) {
@@ -39,5 +60,34 @@ void awaitConnection(SimpleSerialProtocol ssp, char InitChar) {
       ssp.writeChar(InitChar);
     }
   }
+}
+
+bool recv_motor_values(SimpleSerialProtocol ssp, uint16_t motor_vals[NumMotors]) {
+  err = 0;
+  char message_start = ssp.readChar();
+  
+  // Check for error in reading
+  if (message_start != 'M' || err) {
+    // If reading failed or start character is wrong, reset err_num and return.
+    err = 0;
+    return false;
+  }
+
+  motor_vals[0] = ssp.readUnsignedInt16();
+  if (err) { err = 0; return false; }
+  motor_vals[1] = ssp.readUnsignedInt16();
+  if (err) { err = 0; return false; }
+  motor_vals[2] = ssp.readUnsignedInt16();
+  if (err) { err = 0; return false; }
+  motor_vals[3] = ssp.readUnsignedInt16();
+  if (err) { err = 0; return false; }
+  motor_vals[4] = ssp.readUnsignedInt16();
+  if (err) { err = 0; return false; }
+  motor_vals[5] = ssp.readUnsignedInt16();
+  if (err) { err = 0; return false; }
+  motor_vals[6] = ssp.readUnsignedInt16();
+  if (err) { err = 0; return false; }
+
+  return true;
 }
 
