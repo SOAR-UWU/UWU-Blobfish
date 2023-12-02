3be00f1953a028ba490e784b6cf9714eef69ae6b
javin_e <javinenghp@gmail.com>
2023-12-02 23:59:25 +0800

    fix: update test code to follow new structure

--- uwu_main/tests/arduino_test/arduino_test.ino
+++ uwu_main/tests/arduino_test/arduino_test.ino
@@ -1,35 +1,36 @@
+/**
+ * Sketch for testing of Jetson interface functions. To compile this, link with the header files in
+ * the /arduino folder in the root of this repo (e.g. with arduino-cli, using the --library option to
+ * point at the /arduino folder).
+*/
+
 #include <SimpleSerialProtocol.h>
 #include <stdint.h>
-#include "arduino_error_codes.h"
+#include "arduino_error_blinker.h"
 #include "arduino_jetson_interface.h"
-
 
 // Constants
 // TODO read constants from a config file instead of hardcoding
 const int BaudRate = 9600;
-const int CharTimeout = 500;  // waittime for timeout between chars, in ms
+const int CharTimeout = 10;  // waittime for timeout between chars, in ms
 const char InitChar = 'S'; // setup char
 
 #define NUM_MOTORS 7
 
-uint8_t err = 0;
-
-// Functions
 void onError(uint8_t err_num);
-void awaitConnection(SimpleSerialProtocol ssp, char InitChar);
-
 SimpleSerialProtocol ssp{Serial, BaudRate, CharTimeout, onError, 'a', 'z'};
+ArdJetInterface aji {ssp, LED_BUILTIN};
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
   ssp.init();
-  awaitConnection(ssp, InitChar);
+  aji.awaitConnection(InitChar);
 }
 
 void loop() {
   uint16_t motor_values[7];
-  bool received = recv_motor_values(ssp, motor_values);
+  bool received = aji.recv_motor_values(motor_values);
   if (received) {
     ssp.writeChar('M');
     for (auto val : motor_values) {
@@ -39,16 +40,7 @@ void loop() {
 }
 
 void onError(uint8_t err_num) {
-  switch (err_num)
-  {
-  case ERROR_WAIT_FOR_BYTE_TIMEOUT:
-    BlinkPatterns::blink_timeout();   
-    err = 1;
-    break;
-  default:
-    BlinkPatterns::blink_error();
-    break;
-  }
+  aji.onError(err_num);
 }
 
 
