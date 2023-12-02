b83be06b5013204d97b510c5c3d1f546b5e620e0
javin_e <javinenghp@gmail.com>
2023-12-03 01:21:30 +0800

    test: add documentation for arduino interface tests

--- uwu_main/tests/arduino_interface_test.py
+++ uwu_main/tests/arduino_interface_test.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 from import_context import arduino_interface as interface
 import serial
@@ -11,30 +12,44 @@ ARDUINO_SOURCE_FILES = "../../arduino"
 STARTUP_CHAR = b"S"
 
 
+"""Startup function for tests. Compiles Arduino code before upload.
+"""
 @pytest.fixture(scope="module")
 def startup():
     print("Compiling arduino code...")
     subprocess.run(["arduino-cli", "compile", "--fqbn", BOARD_FQBN, ARDUINO_TEST_INO, "--library", ARDUINO_SOURCE_FILES], check=True)
     print("Compiled")
 
+"""Test the connection handshake with Arduino. Fails if connection is not established after 30 seconds.
+"""
 def test_connection_init(startup):
+    timeout = 30
+
     subprocess.run(["arduino-cli", "upload", "-p", ARDUINO_PORT, "--fqbn", BOARD_FQBN, ARDUINO_TEST_INO], check=True)
     print("Uploaded to board.")
-    ser = serial.Serial(ARDUINO_PORT, timeout=30)
+    ser = serial.Serial(ARDUINO_PORT, timeout)
     assert(interface.wait_for_connection(ser, STARTUP_CHAR, 30))
     ser.close()
 
+"""Test the sending of 1 set of motor values to Arduino.
+"""
 def test_send_motor_values(startup):
+    serial_timeout = 5
+    timeout = 30
+    
     subprocess.run(["arduino-cli", "upload", "-p", ARDUINO_PORT, "--fqbn", BOARD_FQBN, ARDUINO_TEST_INO], check=True)
     print("Uploaded to board.")
-    ser = serial.Serial(ARDUINO_PORT, timeout = 5)
+    ser = serial.Serial(ARDUINO_PORT, serial_timeout)
     vals = [133, 434, 512, 123, 364, 125, 126]
-    interface.wait_for_connection(ser, STARTUP_CHAR, 30)
+    assert(interface.wait_for_connection(ser, STARTUP_CHAR, 30))
     interface.write_motor_values(ser, vals)
     recv = []
 
-    # Block until "M" is read
-    while ser.read() != b'M': pass
+    start_time = time.time()
+    # Block until "M" is read, or timeout
+    while ser.read() != b'M':
+        if time.time() - start_time > timeout:
+            pytest.fail("Timed out while awaiting response from Arduino")
 
     for _ in vals:
         recv.append(interface.read_uint16(ser))
