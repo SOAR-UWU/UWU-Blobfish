8b7808b2a19cb3a481aa772307d94aeb72ca0c61
javin_e <javinenghp@gmail.com>
2023-12-02 17:31:17 +0800

    refactor: create class for easier use

--- arduino/arduino_jetson_interface.h
+++ arduino/arduino_jetson_interface.h
@@ -1,6 +1,7 @@
 #pragma once
+#include <stdint.h>
 #include <SimpleSerialProtocol.h>
-#include "arduino_error_codes.h"
+#include "arduino_error_blinker.h"
 
 #ifdef NUM_MOTORS
 const int NumMotors = NUM_MOTORS;
@@ -8,46 +9,84 @@ const int NumMotors = NUM_MOTORS;
 const int NumMotors = 7;
 #endif
 
-extern uint8_t err; // there's probably a better way to do this. Take a look at the ssp errors if you can think of something
 
-void awaitConnection(SimpleSerialProtocol ssp, char InitChar) {
-  while (true) {
-    char rec = ssp.readChar();
-    if (rec == InitChar) {
-      digitalWrite(LED_BUILTIN, LOW);
-      break;
-    } else {
-      BlinkPatterns::blink_connecting();
-      ssp.writeChar(InitChar);
+class ArdJetInterface {
+  public:
+  ArdJetInterface(SimpleSerialProtocol sp, byte led_num) : ssp(sp), blinker(led_num) {}
+  
+  void awaitConnection(char InitChar) {
+    while (true) {
+      char rec = ssp.readChar();
+      if (rec == InitChar) {
+        blinker.set(LOW);
+        break;
+      } else {
+        blinker.blink_connecting();
+        ssp.writeChar(InitChar);
+      }
     }
   }
-}
 
-bool recv_motor_values(SimpleSerialProtocol ssp, uint16_t motor_vals[NumMotors]) {
-  err = 0;
-  char message_start = ssp.readChar();
-  
-  // Check for error in reading
-  if (message_start != 'M' || err) {
-    // If reading failed or start character is wrong, reset err_num and return.
+  bool recv_motor_values(uint16_t motor_vals[NumMotors]) {
     err = 0;
-    return false;
+    char message;
+    message = ssp.readChar();
+    
+    // Check for error in reading
+    if (message != 'M' || err) {
+      // If reading failed or start character is wrong, reset err_num and return.
+      err = 0;
+      putback = message;
+      return false;
+    }
+
+    motor_vals[0] = ssp.readUnsignedInt16();
+    if (err) { err = 0; return false; }
+    motor_vals[1] = ssp.readUnsignedInt16();
+    if (err) { err = 0; return false; }
+    motor_vals[2] = ssp.readUnsignedInt16();
+    if (err) { err = 0; return false; }
+    motor_vals[3] = ssp.readUnsignedInt16();
+    if (err) { err = 0; return false; }
+    motor_vals[4] = ssp.readUnsignedInt16();
+    if (err) { err = 0; return false; }
+    motor_vals[5] = ssp.readUnsignedInt16();
+    if (err) { err = 0; return false; }
+    motor_vals[6] = ssp.readUnsignedInt16();
+    if (err) { err = 0; return false; }
+
+    ssp.readEot();
+    if (err) { err = 0; return false; }
+
+    return true;
+  }
+  
+  void onError(uint8_t err_num) {
+    switch (err_num)
+    {
+    case ERROR_WAIT_FOR_BYTE_TIMEOUT:
+      blinker.blink_timeout();
+      err = 1;
+      break;
+    default:
+      blinker.blink_generic();
+      break;
+    }
   }
 
-  motor_vals[0] = ssp.readUnsignedInt16();
-  if (err) { err = 0; return false; }
-  motor_vals[1] = ssp.readUnsignedInt16();
-  if (err) { err = 0; return false; }
-  motor_vals[2] = ssp.readUnsignedInt16();
-  if (err) { err = 0; return false; }
-  motor_vals[3] = ssp.readUnsignedInt16();
-  if (err) { err = 0; return false; }
-  motor_vals[4] = ssp.readUnsignedInt16();
-  if (err) { err = 0; return false; }
-  motor_vals[5] = ssp.readUnsignedInt16();
-  if (err) { err = 0; return false; }
-  motor_vals[6] = ssp.readUnsignedInt16();
-  if (err) { err = 0; return false; }
+  uint8_t check_error() {
+    return err;
+  }
 
-  return true;
-}
+  char check_last_char() {
+    return putback;
+  }
+
+  public:
+  ErrorBlinker blinker;
+
+  private:
+  char putback;
+  uint8_t err; 
+  SimpleSerialProtocol ssp;
+};
