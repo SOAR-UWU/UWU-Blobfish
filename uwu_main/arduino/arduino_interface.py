a52a8e7d78afa812e5679a8876923906f40db0cc
javin_e <javinenghp@gmail.com>
2023-12-03 15:11:06 +0800

    refactor: make interface into class

--- uwu_main/arduino/arduino_interface.py
+++ uwu_main/arduino/arduino_interface.py
@@ -9,101 +9,113 @@ import serial
 NUM_MOTORS = 7
 
 EOT_CHAR = b'\n'
+MOTOR_VALUE_START = b'M'
 
-def await_eot(ser: serial.Serial, timeout):
-    start_time = time.time()
-    while True:
-        if ser.read() == EOT_CHAR:
-            return True
-
-        current = time.time()
-        if current - start_time > timeout:
-            return False
-
-def write_motor_values(ser: serial.Serial, vals: List[int], endianness: str = 'l'):
-    """Write 7 motor values to the serial port (in the form of 7 uint16s in a row).
-
-    Args:
-        ser (serial.Serial): Serial object to write to
-        vals (List[int]): list of motor values
-        endianness (str, optional): Endian-ness setting, 'b' for big and 'l' for little. 
-            Defaults to 'l'.
-    """
-    if len(vals) != NUM_MOTORS:
-        raise ValueError(f"Length of motor values should be the same as the number of motors ({NUM_MOTORS})")
+class JetsonArduinoInterface:
+    def __init__(self, ser: serial.Serial, endianness: str = 'l'):
+        self.ser = ser
+        self.endianness = endianness
     
-    # First write the init message
-    ser.write(b'M')
+    @property
+    def endianness(self):
+        return self._endianness
 
-    # Then write motor values
-    for val in vals:
-        write_uint16(ser, val, endianness)
-
-    # Write EOT
-    ser.write(EOT_CHAR)
-
-def write_uint16(ser: serial.Serial, n: int, endianness: str = 'l'):
-    """Write an integer to the serial port as an unsigned 16-bit integer.
-
-    Args:
-        n (int): Number to write
-        endianness (str): Character representing endian-ness - 'b' for big and
-            'l' for little. Default is little.
-    """
-    _write_bytes(ser, n, endianness, 2)
-
-def read_uint16(ser: serial.Serial, endianness: str = 'l'):
-    return _read_bytes(ser, endianness, 2)
-
-def read_uint64(ser: serial.Serial, endianness: str = 'l'):
-    return _read_bytes(ser, endianness, 4)
-
-def _read_bytes(ser: serial.Serial, endianness: str, n_bytes: int):
-    read_b = ser.read(n_bytes)
-    if endianness == 'l':
-        ret = int.from_bytes(read_b, 'little')
-    else:
-        ret = int.from_bytes(read_b, 'big')
+    @endianness.setter
+    def endianness(self, endian: str):
+        if endian != 'b' and endian != 'l':
+            raise ValueError(f"Endianness should be represented as either 'b' or 'l', not {endian}")
+        self._endianness = endian
     
-    return ret
+    def await_eot(self, timeout):
+        """Wait for the EOT signal for timeout seconds.
 
-def _write_bytes(ser: serial.Serial, n: int, endianness: str, n_bytes: int):
-    if endianness == 'l':
-        byte_n = n.to_bytes(n_bytes, 'little')
-    else:
-        byte_n = n.to_bytes(n_bytes, 'big')
-    ser.write(byte_n)
+        Args:
+            timeout (int): seconds to wait for eot
 
-def wait_for_connection(ser: serial.Serial, startup_char: bytes, timeout: int):
-    """Test for connection with Arduino.
+        Returns:
+            bool: True if EOT is read, False if not
+        """
+        start_time = time.time()
+        while True:
+            if self.ser.read() == EOT_CHAR:
+                return True
 
-    Blocks until either connection is established with the Arduino or the
-    timeout is reached. Returns True when connected; False if timed out.
+            current = time.time()
+            if current - start_time > timeout:
+                return False
 
-    Args:
-        timeout (int): Time to wait in seconds. Set to 0 or less to never
-        timeout.
-    """
-    start = time.time()
-    while ser.read() != startup_char:
-        time.sleep(1)
-        current = time.time()
-        if timeout > 0 and current - start > timeout:
-            # timed out, stop attempting to connect
-            return False
+    def write_motor_values(self, vals: List[int]):
+        """Write 7 motor values to the serial port (in the form of 7 uint16s in a row).
 
-    ser.write(startup_char)
-    # Signal received, connected. Return True.
-    return True
+        Args:
+            vals (List[int]): list of motor values
+        """
+        if len(vals) != NUM_MOTORS:
+            raise ValueError(f"Length of motor values should be the same as the number of motors ({NUM_MOTORS})")
+        
+        # First write the init message
+        self.ser.write(MOTOR_VALUE_START)
 
+        # Then write motor values
+        for val in vals:
+            self.write_uint16(val)
 
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0')
-    print(ser.name)
-    while True:
-        write_uint16(ser, 1676)
-        write_uint16(ser, 3451)
-        # ser.write(b'\x0A')
-        print(ser.readline())
-        time.sleep(0.5)
+        # Write EOT
+        self.ser.write(EOT_CHAR)
 
+    def write_uint16(self, n: int):
+        """Write an integer to the serial port as an unsigned 16-bit integer.
+
+        Args:
+            n (int): Number to write
+        """
+        self._write_bytes(n, 2)
+
+    def read_uint16(self):
+        """Read an integer from the serial port as an unsigned integer.
+
+        Returns:
+            int: read integer value
+        """
+        return self._read_bytes(2)
+
+    def read_uint64(self):
+        return self._read_bytes(4)
+
+    def _read_bytes(self, n_bytes: int):
+        read_b = self.ser.read(n_bytes)
+        if self.endianness == 'l':
+            ret = int.from_bytes(read_b, 'little')
+        else:
+            ret = int.from_bytes(read_b, 'big')
+        
+        return ret
+
+    def _write_bytes(self, n: int, n_bytes: int):
+        if self.endianness == 'l':
+            byte_n = n.to_bytes(n_bytes, 'little')
+        else:
+            byte_n = n.to_bytes(n_bytes, 'big')
+        self.ser.write(byte_n)
+
+    def wait_for_connection(self, startup_char: bytes, timeout: int):
+        """Test for connection with Arduino.
+
+        Blocks until either connection is established with the Arduino or the
+        timeout is reached. Returns True when connected; False if timed out.
+
+        Args:
+            timeout (int): Time to wait in seconds. Set to 0 or less to never
+            timeout.
+        """
+        start = time.time()
+        while self.ser.read() != startup_char:
+            time.sleep(1)
+            current = time.time()
+            if timeout > 0 and current - start > timeout:
+                # timed out, stop attempting to connect
+                return False
+
+        self.ser.write(startup_char)
+        # Signal received, connected. Return True.
+        return True
