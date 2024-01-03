f0759a460ec679f0bc65fbe07e0f92f3fe2346e8
javin_e <javinenghp@gmail.com>
2024-01-03 15:19:53 +0800

    feat: thruster manager initial

--- uwu_main/src/thruster_manager/thruster_manager/thruster_manager.py
+++ uwu_main/src/thruster_manager/thruster_manager/thruster_manager.py
@@ -2,6 +2,7 @@ from simple_pid.pid import PID
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Vector3
+from motor_msg.msg import Motors
 import numpy as np
 
 class Thruster_Manager(Node):
@@ -21,6 +22,7 @@ class Thruster_Manager(Node):
 
         self.scale = 300    # how much to scale the pid value
         self.create_subscription(Vector3, 'pid_node/yawpitchroll_pid', self.calculate_thrusters, 10)
+        self.motor_publisher = self.create_publisher(Motors, 'motor_values', 10)
         self.get_logger().info("Thruster manager started")
 
     
@@ -31,4 +33,26 @@ class Thruster_Manager(Node):
             [pid_msg.z]
         ])
         pid_vector *= self.scale
-        outputs = 
+        outputs = self.motor_matrix @ pid_vector
+        outputs += 1500
+        motor_vals = Motors()
+        motor_vals.motor1 = outputs[0]
+        motor_vals.motor2 = outputs[1]
+        motor_vals.motor3 = outputs[2]
+        motor_vals.motor4 = outputs[3]
+        motor_vals.motor5 = outputs[4]
+        motor_vals.motor6 = outputs[5]
+        motor_vals.motor7 = outputs[6]
+        self.motor_publisher.publish(motor_vals)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    manager = Thruster_Manager()
+    rclpy.spin(manager)
+
+    manager.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
