365b2d89241eafdbc89aac27d6b131578b86cdf3
KoB4N <tankovan@gmail.com>
2024-01-03 16:19:34 +0800

    calibration_v2

--- uwu_main/src/simple_pid/simple_pid/pid_offset_calibration.py
+++ uwu_main/src/simple_pid/simple_pid/pid_offset_calibration.py
@@ -2,44 +2,45 @@ from simple_pid.pid import PID
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Vector3
-import time
 
 class Offset_Calibration(Node):
     def __init__(self):
         super().__init__("offset_calibrator")
 
         self.create_subscription(Vector3, 'imu/yawpitchroll', self.callback)
-        
-        self.imu_data_list = []
-
-        self.declare_parameter('imu_data_count', 0)
+        self.imu_data_collection = 0
+        self.count = 0
+        self.declare_parameter('imu_data_count', 4000)
         self.imu_data_count = self.get_parameter('imu_data_count')
-
         self.declare_parameter('imu_average_value', 0.0)
         self.average_value = self.get_parameter('imu_average_value')
 
+        self.imu_offset_publisher = self.create_publisher(Vector3, 'imu/corrected_yawpitchroll', 10)
+
 
     def callback(self, msg):
-        self.imu_data = msg.x
-        self.imu_data_list.append(self.imu_data)
+        if self.count < self.imu_data_count:
+            self.imu_data_collection += msg.x
+            self.count+=1
+            
+        elif self.count == self.imu_data_count:
+            self.average_value.set_value(self.imu_data_collection / self.imu_data_count)
+            self.get_logger().info(f"The average calibrated IMU offset value is: {self.average_value}")
+            self.count+=1
 
- 
-    def record_imu_data(self):
-        total_value_count = 4000
+        else:
+            pid_values = Vector3()
 
-        for count in range(total_value_count):
-            rclpy.spin_once(self)
-            self.imu_data_count.set_value(count)
-            time.sleep(0.01)
+            pid_values.x = msg.x - self.average_value
+            pid_values.y = msg.y
+            pid_values.z = msg.z
+            self.imu_offset_publisher.publish(pid_values)
+        
 
-        self.average_value.set_value(sum(self.imu_data_list) / total_value_count)
-        print(f"The average calibrated IMU offset value is: {self.average_value}")
-
-    
 def main():
     rclpy.init()
     node = Offset_Calibration()
-    node.record_imu_data()
+    rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
 
