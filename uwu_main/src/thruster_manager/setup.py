a4350f79860bfce6b04bec4db1c7e3430e29449e
javin_e <javinenghp@gmail.com>
2024-01-03 18:38:41 +0800

    fix: add entrypoint for thruster manager

--- uwu_main/src/thruster_manager/setup.py
+++ uwu_main/src/thruster_manager/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "thruster_manager = thruster_manager.thruster_manager:main"
         ],
     },
 )
