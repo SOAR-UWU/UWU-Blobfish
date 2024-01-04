5e81ffc2600502075cc92143d74bee4fc61bb18d
soar-uwu <sutdunderwaterunit@gmail.com>
2024-01-04 11:35:36 +0800

    feat: launch file

---
+++ uwu_main/launch.py
@@ -0,0 +1,42 @@
+import os
+from sys import executable
+
+from ament_index_python.packages import get_package_share_directory
+
+from uwu_main.launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+
+    # Include vectornav launch file
+    vn_pkg = get_package_share_directory('vectornav')
+    vn_launch_dir = os.path.join(vn_pkg, 'launch')
+    vn_launch = IncludeLaunchDescription(
+      PythonLaunchDescriptionSource(os.path.join(vn_launch_dir, 'vectornav.launch.py'))
+    )
+
+    # pid node
+    pid_node = Node(
+        package="pid_package",
+        executable="pid_node"
+    )
+
+    pid_calibration = Node(
+        package="pid_package",
+        executable="offset_calibrator"
+    )
+
+    motor_bridge = Node(
+        package="arduino_bridge",
+        executable="bridge"
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+    ld.add_action(vn_launch)
+    ld.add_action(pid_node)
+    ld.add_action(pid_calibration)
+    ld.add_action(motor_bridge)
+    return ld
