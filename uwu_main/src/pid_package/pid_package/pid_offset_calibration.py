9b2704128f933b251ad5839f950d834864413a91
soar-uwu <sutdunderwaterunit@gmail.com>
2024-01-04 13:11:51 +0800

    fix: pid offset calibrator fixes

--- uwu_main/src/pid_package/pid_package/pid_offset_calibration.py
+++ uwu_main/src/pid_package/pid_package/pid_offset_calibration.py
@@ -1,35 +1,39 @@
 from simple_pid.pid import PID
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Quaternion, Vector3
+from geometry_msgs.msg import PoseWithCovarianceStamped, Vector3
 from tf_transformations import euler_from_quaternion
+from rclpy import Parameter
 
 class Offset_Calibration(Node):
     def __init__(self):
         super().__init__("offset_calibrator")
 
         qos_profile = rclpy.qos.qos_profile_sensor_data
-        self.create_subscription(Vector3, 'vectornav/pose', self.callback, qos_profile)
+        self.create_subscription(PoseWithCovarianceStamped, '/vectornav/pose', self.callback, qos_profile)
         
         self.imu_data_collection = 0
         self.count = 0
-        self.declare_parameter('imu_data_count', 4000)
-        self.imu_data_count = self.get_parameter('imu_data_count')
-        self.declare_parameter('imu_average_value', 0.0)
-        self.average_value = self.get_parameter('imu_average_value')
+        self.declare_parameter('imu_data_count', 100)
+        self.imu_data_count = self.get_parameter('imu_data_count').value
+        self.average_value = 0.0
 
         self.imu_offset_publisher = self.create_publisher(Vector3, 'imu/corrected_yawpitchroll', 10)
+        self.get_logger().info("offset calibrator started")
 
 
     def callback(self, msg):
-        euler_angles = euler_from_quaternion([msg.x, msg.y, msg.z, msg.w])
+        quat = msg.pose.pose.orientation
+        euler_angles = euler_from_quaternion([quat.w, quat.x, quat.y, quat.z])
 
         if self.count < self.imu_data_count:
             self.imu_data_collection += euler_angles[0]
             self.count+=1
             
         elif self.count == self.imu_data_count:
-            self.average_value.set_value(self.imu_data_collection / self.imu_data_count)
+            self.average_value = self.imu_data_collection / self.imu_data_count
+            # ave_val = Parameter("imu_average_value", Parameter.Type.DOUBLE, self.imu_data_collection / self.imu_data_count)
+            # self.set_parameters([ave_val])
             self.get_logger().info(f"The average calibrated IMU offset value is: {self.average_value}")
             self.count+=1
 
