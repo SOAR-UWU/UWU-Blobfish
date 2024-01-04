008612d6ab3ab8b9f9c3864b07e3762a7edc31f1
soar-uwu <sutdunderwaterunit@gmail.com>
2024-01-04 15:21:38 +0800

    fix: correct motor matrix

--- uwu_main/src/thruster_manager/thruster_manager/thruster_manager.py
+++ uwu_main/src/thruster_manager/thruster_manager/thruster_manager.py
@@ -11,17 +11,17 @@ class Thruster_Manager(Node):
         
         self.motor_matrix = np.array([
             # yaw pitch roll
-            [1,    0,    0],    # front left motor
-            [-1,   0,    0],    # front right motor
-            [0,    0,    1],    # middle left motor
-            [0,    0,   -1],    # middle right motor
-            [-1,   0,    0],    # back left motor
-            [1,    0,    0],    # back right motor
-            [0,    1,    0],    # back middle motor
+            [1,    0,    0],    # front right motor
+            [-1,   0,    0],    # back right motor
+            [0,   -1,    0],    # back middle motor
+            [1,    0,    0],    # back left motor
+            [0,    0,   -1],    # middle left motor
+            [0,    0,    1],    # middle right motor
+            [-1,   0,    0],    # front left motor
         ])
 
         self.scale = 300    # how much to scale the pid value
-        self.create_subscription(Vector3, 'pid_node/yawpitchroll_pid', self.calculate_thrusters, 10)
+        self.create_subscription(Vector3, '/yawpitchroll_pid', self.calculate_thrusters, 10)
         self.motor_publisher = self.create_publisher(Motors, 'motor_values', 10)
         self.get_logger().info("Thruster manager started")
 
@@ -36,13 +36,13 @@ class Thruster_Manager(Node):
         outputs = self.motor_matrix @ pid_vector
         outputs += 1500
         motor_vals = Motors()
-        motor_vals.motor1 = outputs[0]
-        motor_vals.motor2 = outputs[1]
-        motor_vals.motor3 = outputs[2]
-        motor_vals.motor4 = outputs[3]
-        motor_vals.motor5 = outputs[4]
-        motor_vals.motor6 = outputs[5]
-        motor_vals.motor7 = outputs[6]
+        motor_vals.motor_fr = int(outputs[0])
+        motor_vals.motor_mr = int(outputs[1])
+        motor_vals.motor_bm = int(outputs[2])
+        motor_vals.motor_bl = int(outputs[3])
+        motor_vals.motor_ml = int(outputs[4])
+        motor_vals.motor_br = int(outputs[5])
+        motor_vals.motor_fl = int(outputs[6])
         self.motor_publisher.publish(motor_vals)
 
 def main(args=None):
