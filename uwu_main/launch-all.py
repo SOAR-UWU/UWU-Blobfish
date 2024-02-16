3da549f385d3a30f2f541cf1a977bdc9fcec86cc
javin_e <javinenghp@gmail.com>
2024-02-16 10:40:38 +0800

    fix: add config loading to launch-all.py

--- uwu_main/launch-all.py
+++ uwu_main/launch-all.py
@@ -18,30 +18,38 @@ def generate_launch_description():
       PythonLaunchDescriptionSource(os.path.join(vn_launch_dir, 'vectornav.launch.py'))
     )
 
+    pid_configs = os.path.join(get_package_share_directory('pid_package'), 'config', "params.yaml")
+    arduino_configs = os.path.join(get_package_share_directory('arduino_bridge'), 'config', "params.yaml")
+    thruster_manager_configs = os.path.join(get_package_share_directory('thruster_manager'), 'config', "params.yaml")
+
     # pid node
     pid_node = Node(
         package="pid_package",
-        executable="pid_node"
+        executable="pid_node",
+        parameters=[pid_configs]
     )
 
     pid_calibration = Node(
         package="pid_package",
-        executable="offset_calibrator"
+        executable="offset_calibrator",
+        parameters=[pid_configs]
     )
 
     pid_motor_dir_control = Node(
-        package="pid_package",
+        package="teleop",
         executable="motor_dir_control_node"
     )
 
     thruster_manager_node = Node(
         package="thruster_manager",
-        executable="thruster_manager"
+        executable="thruster_manager",
+        parameters=[thruster_manager_configs]
     )
 
     motor_bridge = Node(
         package="arduino_bridge",
-        executable="bridge"
+        executable="bridge",
+        parameters=[arduino_configs]
     )
 
     # Create the launch description and populate
