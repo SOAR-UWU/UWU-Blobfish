dfe97e181ce3a32a9451bc02fb5198ca20080764
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-02-21 21:41:53 +0800

    make our own .isaac_ros_common-config

--- activate.sh
+++ activate.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
 
-# PUT .isaac_ros_common-config OPTIONS HERE
-export CONFIG_IMAGE_KEY="ros2_humble.fixes.user.realsense.arduino.gazebo.udev.misc"
-export CONFIG_DOCKER_SEARCH_DIRS=(../../../docker) # RELATIVE TO run_dev.sh
+if [[ -f "${dir}/.isaac_ros_common-config" ]]; then
+    . "${dir}/.isaac_ros_common-config"
+fi
 
 $dir/thirdparty/isaac_ros_common/scripts/run_dev.sh $dir
