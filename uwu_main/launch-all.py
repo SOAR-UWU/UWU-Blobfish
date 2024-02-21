0b13fb1cf7b83184d2a1b15228a9661f1cfac342
javin_e <javinenghp@gmail.com>
2024-02-21 20:26:35 +0800

    feat: add hardware available cmdline option

--- uwu_main/launch-all.py
+++ uwu_main/launch-all.py
@@ -1,13 +1,20 @@
 import os
+import sys
 from sys import executable
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 
+hardware_available = True
+
+for arg in sys.argv:
+    if arg == 'hardware_available:=false':
+        hardware_available = False
 
 def generate_launch_description():
 
@@ -54,11 +61,13 @@ def generate_launch_description():
     )
 
     # Create the launch description and populate
+    # if hardware is not available, do not attempt to launch the IMU and Arduino nodes
     ld = LaunchDescription()
-    ld.add_action(vn_launch)
+    if hardware_available:
+        ld.add_action(vn_launch)
+        ld.add_action(motor_bridge)
     ld.add_action(pid_calibration)
     ld.add_action(pid_node)
     ld.add_action(pid_motor_dir_control)
     ld.add_action(thruster_manager_node)
-    ld.add_action(motor_bridge)
     return ld
