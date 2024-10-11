4477bf153934ec1530f656f3d7a11285bfe1ba89
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-10-11 11:53:50 +0800

    add more checks to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,22 @@
 
 set -eu
 
+# Make sure we are on Ubuntu/Ubuntu WSL, not git bash or other distros.
+if [[ $(source /etc/os-release && echo $ID) != "ubuntu" ]]; then
+    echo "This script is only supported on Ubuntu/Ubuntu WSL."
+    echo "You are on: $(source /etc/os-release && echo $ID)"
+    exit 1
+fi
+
+# Force script to run as root.
+# NOTE: What if some scripts need the current user? Better to use `sudo` when needed.
+# This has the downside that if the script runs for a long time, the password might
+# have to be entered again.
+# if [[ $(id -u) -ne 0 ]]; then
+#     echo Please run this script as root or using sudo!
+#     exit 1
+# fi
+
 # Directory this script is in.
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Directory of workspace root, assuming script is in `scripts/`.
