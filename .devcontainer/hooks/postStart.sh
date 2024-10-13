cc971cf64e9842e8e337966b930acd747f1fb634
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-10-13 10:43:58 +0000

    fix WSL iGPU not detected/working

--- .devcontainer/hooks/postStart.sh
+++ .devcontainer/hooks/postStart.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 # Insert commands to run every time the container is started.
+# Unfortunately, WSL's GID for render is inconsistent so I can't "group_add" the user.
+sudo chmod 777 /dev/dri/renderD128
