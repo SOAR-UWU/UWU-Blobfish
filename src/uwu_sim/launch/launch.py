4fc8472fb43b243d36b04ea33ac28315aedbdaf0
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-11-02 19:54:35 +0000

    refactor all bridge stuff to launch file in blobfish_sim

--- src/uwu_sim/launch/launch.py
+++ src/uwu_sim/launch/launch.py
@@ -7,7 +7,6 @@ for list of available launch functions.
 from ament_index_python.packages import get_package_share_path
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch_ros.actions import Node
 
 WORLD_LAUNCH_FILE = "pool_world.launch.py"
 ROBOT_TYPE = "blobsimp"
@@ -15,32 +14,21 @@ ROBOT_TYPE = "blobsimp"
 
 def generate_launch_description():
     pkg_uwu_sim = get_package_share_path("uwu_sim")
+    pkg_blobfish_sim = get_package_share_path("blobfish_sim")
 
     xacro = IncludeLaunchDescription(
         str(pkg_uwu_sim / "launch" / "xacro.launch.py"),
         launch_arguments={"robot": ROBOT_TYPE}.items(),
     )
     world = IncludeLaunchDescription(str(pkg_uwu_sim / "launch" / WORLD_LAUNCH_FILE))
-    gz_topic_bridge = Node(
-        package="ros_gz_bridge",
-        executable="parameter_bridge",
-        parameters=[{"config_file": str(pkg_uwu_sim / "config" / "bridge.yaml")}],
-    )
-    keypress_bridge = Node(package="blobfish_sim", executable="sim_keypress")
-    imu_bridge = Node(package="blobfish_sim", executable="sim_imu")
-    depth_bridge = Node(package="blobfish_sim", executable="sim_depth")
-    arduino_bridge = Node(package="blobfish_sim", executable="sim_arduino")
+    bridge = IncludeLaunchDescription(str(pkg_blobfish_sim / "launch" / "launch.py"))
     robot = IncludeLaunchDescription(str(pkg_uwu_sim / "launch" / "robot.launch.py"))
 
     return LaunchDescription(
         [
             xacro,
             world,
-            gz_topic_bridge,
-            keypress_bridge,
-            imu_bridge,
-            depth_bridge,
-            arduino_bridge,
+            bridge,
             robot,
         ]
     )
