e5325d7c3a4968d0f50ced9feba6b6f47c9204cf
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-11-04 06:48:27 +0000

    feat xacro launch file xacro everything

--- src/uwu_sim/launch/xacro.launch.py
+++ src/uwu_sim/launch/xacro.launch.py
@@ -1,57 +1,26 @@
-"""Launch file to compile robot's xacro and other robot things."""
+"""Launch file that compiles all .xacro files found in place."""
 
 from ament_index_python.packages import get_package_share_path
 from launch import LaunchDescription
-from launch.actions import (
-    DeclareLaunchArgument,
-    ExecuteProcess,
-    # LogInfo,
-)
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
-
-ROBOT_TYPE = "blobsimp"
-ROBOT_XACRO_NAME = "model.sdf.xacro"
-ROBOT_SDF_NAME = "model.sdf"
-
-
-def declare_launch_arguments():
-    """Documentation of available arguments go here."""
-    robot_type = DeclareLaunchArgument("robot", default_value=ROBOT_TYPE)
-    return [robot_type]
+from launch.actions import ExecuteProcess, LogInfo
 
 
 def generate_launch_description():
     pkg_uwu_sim = get_package_share_path("uwu_sim")
-    arg_robot_type = LaunchConfiguration("robot")
 
-    # Reads stdout from xacro
-    stdout_xacro = Command(
-        [
-            "xacro ",
-            PathJoinSubstitution(
-                [str(pkg_uwu_sim / "robots"), arg_robot_type, ROBOT_XACRO_NAME]
-            ),
-        ]
-    )
+    ld = LaunchDescription()
+    for path in pkg_uwu_sim.rglob("*.xacro"):
+        out_path = path.with_suffix("")
+        log1 = LogInfo(msg=f"Found xacro: {path}")
+        compile = ExecuteProcess(
+            name="xacro",
+            cmd=["xacro", str(path), ">", str(out_path)],
+            shell=True,
+        )
+        log2 = LogInfo(msg=f"Compiled xacro: {out_path}")
 
-    # Write out the xacro file to update it
-    write_xacro = ExecuteProcess(
-        name="write_xacro",
-        cmd=[
-            "echo",
-            ["'", stdout_xacro, "'"],
-            ">",
-            PathJoinSubstitution(
-                [str(pkg_uwu_sim / "robots"), arg_robot_type, ROBOT_SDF_NAME]
-            ),
-        ],
-        shell=True,
-    )
+        ld.add_action(log1)
+        ld.add_action(compile)
+        ld.add_action(log2)
 
-    return LaunchDescription(
-        [
-            *declare_launch_arguments(),
-            write_xacro,
-            # LogInfo(msg=xacro_cmd),
-        ]
-    )
+    return ld
