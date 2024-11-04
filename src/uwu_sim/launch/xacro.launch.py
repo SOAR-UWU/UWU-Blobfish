68bda89618692b22307b34ce7c50f901cf3d4ab8
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-11-04 10:14:51 +0000

    add ignore list to xacro

--- src/uwu_sim/launch/xacro.launch.py
+++ src/uwu_sim/launch/xacro.launch.py
@@ -8,12 +8,34 @@ from ament_index_python.packages import get_package_share_path
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, LogInfo
 
+IGNORE = [
+    "robots/beluga",
+]
+
+
+def build_ignore_set():
+    pkg_uwu_sim = get_package_share_path("uwu_sim")
+    ignore_set = set()
+    for entry in IGNORE:
+        path = pkg_uwu_sim / entry
+        if path.is_file():
+            ignore_set.add(path)
+            continue
+
+        for xacro in (pkg_uwu_sim / entry).rglob("*.xacro"):
+            ignore_set.add(xacro)
+    return ignore_set
+
 
 def generate_launch_description():
     pkg_uwu_sim = get_package_share_path("uwu_sim")
+    ignore = build_ignore_set()
 
     ld = LaunchDescription()
     for path in pkg_uwu_sim.rglob("*.xacro"):
+        if path in ignore:
+            continue
+
         out_path = path.with_suffix("")
         log1 = LogInfo(msg=f"Found xacro: {path}")
         compile = ExecuteProcess(
