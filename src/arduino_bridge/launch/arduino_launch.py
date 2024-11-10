1bc53331310b00d5eee9124948413e643f8afb79
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-11-10 15:16:45 +0000

    feat: theoretically auto-detect arduino

--- src/arduino_bridge/launch/arduino_launch.py
+++ src/arduino_bridge/launch/arduino_launch.py
@@ -1,10 +1,21 @@
 from ament_index_python.packages import get_package_share_path
 from launch import LaunchDescription
+from launch.actions import LogInfo
 from launch_ros.actions import Node
+from serial.tools import list_ports
+from serial.tools.list_ports_common import ListPortInfo
+
+ARDUINO_DEV_ID = "1a86:7523"
 
 
 def generate_launch_description():
     pkg_this = get_package_share_path("arduino_bridge")
+
+    # TODO: needs testing.
+    try:
+        dev: ListPortInfo = next(list_ports.grep(ARDUINO_DEV_ID))
+    except StopIteration:
+        return LaunchDescription([LogInfo(msg="Arduino not found, skipping...")])
 
     motor_bridge = Node(
         package="arduino_bridge",
@@ -12,9 +23,11 @@ def generate_launch_description():
         parameters=[
             pkg_this / "config" / "params.yaml",
             {"arduino_files": str(pkg_this / "arduino")},
+            {"arduino_port": dev.device},
         ],
     )
 
     ld = LaunchDescription()
+    ld.add_action(LogInfo(msg=f"Arduino found: {dev.device}"))
     ld.add_action(motor_bridge)
     return ld
