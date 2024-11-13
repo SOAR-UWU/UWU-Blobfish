eeb77d1313f754b81e1e63a1dc14e89efc2277f8
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-11-13 01:21:34 +0000

    feat: diagnostics pkg, motor test node & launch

---
+++ src/diagnostics/setup.py
@@ -0,0 +1,27 @@
+from glob import glob
+
+from setuptools import find_packages, setup
+
+package_name = "diagnostics"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (f"share/{package_name}/launch", glob("launch/*")),
+        (f"share/{package_name}/config", glob("config/*")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="J-H",
+    maintainer_email="42513874+Interpause@users.noreply.github.com",
+    description="Package description",
+    license="License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": ["motor = diagnostics.motor:main"],
+    },
+)
