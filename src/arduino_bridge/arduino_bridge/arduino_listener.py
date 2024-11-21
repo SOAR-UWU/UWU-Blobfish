b4007882504f3afa86f8db4f8d42fb1e62d87ac6
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-11-21 11:51:04 +0000

    fix: rate limit the arduino listener due to 800hz sensor msgs

--- src/arduino_bridge/arduino_bridge/arduino_listener.py
+++ src/arduino_bridge/arduino_bridge/arduino_listener.py
@@ -1,4 +1,5 @@
 import rclpy
+import rclpy.qos
 from rclpy.node import Node
 from std_msgs.msg import Float32
 import os
@@ -10,17 +11,26 @@ from .arduino_interface import JetsonArduinoInterface
 import serial
 import subprocess
 
+# TODO: Benchmark maximum rate possible of sending motor values to Arduino.
+# Napkin maths:
+# 19200 baudrate = 2400 bytes per second
+# 7 motors * 2 bytes per uint16 + 2 bytes start/end = 16 bytes per message
+# 2400 / 16 = 150 messages per second
+MOTOR_UPDATE_RATE = 90
+MOTOR_NEUTRAL = 1500
+MOTOR_TOPIC = "/blobfish/motor_values"
+QOS = rclpy.qos.qos_profile_sensor_data
+
+# TODO: Add magic version number to main.ino so we can detect if necessary to compile
+# and upload new code.
 
 class ArduinoBridge(Node):
     def __init__(self):
         super().__init__("arduino_bridge")
         self.subscription = self.create_subscription(
-            Motors,
-            "blobfish/motor_values",
-            self.listener_callback,
-            10
+            Motors, MOTOR_TOPIC, self.listener_callback, QOS
         )
-        self.depth_publisher = self.create_publisher(Float32, "depth", 10)
+        self.depth_publisher = self.create_publisher(Float32, "depth", 0)
         self.declare_parameter('arduino_files', Parameter.Type.STRING)
         self.declare_parameter('arduino_port', Parameter.Type.STRING)
         self.declare_parameter("baud_rate", Parameter.Type.INTEGER)
@@ -57,15 +67,20 @@ class ArduinoBridge(Node):
         self.jai.wait_for_connection(b'S', 0)
         self.get_logger().info("Connection established, Arduino bridge started")
 
-    def listener_callback(self, msg):
-        self.jai.write_motor_values([
-            msg.motor_1,
-            msg.motor_2,
-            msg.motor_3,
-            msg.motor_4,
-            msg.motor_5,
-            msg.motor_6,
-            msg.motor_7])
+        self.motors = [MOTOR_NEUTRAL] * 7
+        self.create_timer(1 / MOTOR_UPDATE_RATE, self.update_motors)
+
+    def listener_callback(self, msg: Motors):
+        self.motors[0] = msg.motor_1
+        self.motors[1] = msg.motor_2
+        self.motors[2] = msg.motor_3
+        self.motors[3] = msg.motor_4
+        self.motors[4] = msg.motor_5
+        self.motors[5] = msg.motor_6
+        self.motors[6] = msg.motor_7
+        
+    def update_motors(self):
+        self.jai.write_motor_values(self.motors)
         
     def check_depth_value(self):
         depth = self.jai.read_depth_value()
